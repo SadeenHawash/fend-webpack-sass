a29753396897d37c9e0f72b2aee3221554740970
sadeen_hawash <sadeenghawash593@gmail.com>
2025-02-13 22:17:50 +0200

    api call challenge

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,12 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { checkForName } from "./js/nameChecker";
+import { handleSubmit } from "./js/formHandler";
 
 console.log(checkForName);
 
-alert("I EXIST")
+alert("I EXIST");
 console.log("CHANGE!!");
+
+// Add event listener to the form
+document.getElementById("myForm").addEventListener("submit", handleSubmit);
+
+export { checkForName, handleSubmit };
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,50 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault(); // Prevent default form submission
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+  // Get user input from the form
+  let formText = document.getElementById("name").value;
+  Client.checkForName(formText);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+  console.log("::: Form Submitted :::");
+
+  // Fetch data from an external API
+  fetch(`https://jsonplaceholder.typicode.com/posts/1`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      return res.json();
     })
+    .then((data) => {
+      // Display API response in the UI
+      document.getElementById("results").innerHTML = `
+          <h3>${data.title}</h3>
+          <p>${data.body}</p>
+        `;
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      document.getElementById(
+        "results"
+      ).innerHTML = `<p style="color:red;">Error fetching data</p>`;
+    });
 }
 
-export { handleSubmit }
+export { handleSubmit };
+
+// function handleSubmit(event) {
+//     event.preventDefault()
+
+//     // check what text was put into the form field
+//     let formText = document.getElementById('name').value
+//     checkForName(formText)
+
+//     console.log("::: Form Submitted :::")
+//     fetch('http://localhost:8080/test')
+//     .then(res => res.json())
+//     .then(function(res) {
+//         document.getElementById('results').innerHTML = res.message
+//     })
+// }
+
+// export { handleSubmit }
